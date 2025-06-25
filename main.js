e3b038f9b722416eef418c4b556a8ec2af97e130
Alex Pontes <alexsandro.pontes@escola.pr.gov.br>
2025-06-25 11:36:59 -0300

    arrumando a data

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ for(let i=0;i <botoes.length;i++){
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-06-25T00:00:00")
+const tempoObjetivo1 = new Date("2025-25-06T00:00:00")
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
